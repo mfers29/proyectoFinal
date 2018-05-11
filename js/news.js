2044e7505223a4cd523d5b17edac4459256f6df1
mfers29 <32649286+mfers29@users.noreply.github.com>
2018-05-11 18:22:00 +0200

    Add files via upload

---
+++ js/news.js
@@ -0,0 +1,25 @@
+//Variable contador para cargar diferentes json
+var i;
+i=1;
+
+//Empezar una vez esté la página lista
+$(document).ready(function(){
+
+//ocultamos el div
+//$("#plantilla").hide();
+	
+//Evento de llegada al final de la página 
+//$( window ).scroll(function(){
+//      if($(window).scrollTop() + $(window).height() == $( document ).height()) {
+//            alert("Has llegado al final de la página");
+//      }
+//          						});
+								
+//Evento de hacer click el boton
+$( "#botonCargar" ).click(function(){alert("Prueba click")});
+
+$.getJSON('prueba.json',function(noticias){alert(noticias.contenido[1].titulo)});
+ 
+   
+	
+	}); //Cierre de la función de document.ready
